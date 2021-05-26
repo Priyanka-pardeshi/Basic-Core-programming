034c42ae449f860bbf229d66236377b39d3a5ea6
echo # <priyankapardeshi2312@gmail.com>
2021-05-26 14:56:37 +0530

    Chenges in ic statement

--- Basic_Core/Basic_Core/Toss.cs
+++ Basic_Core/Basic_Core/Toss.cs
@@ -11,8 +11,8 @@ namespace Basic_Core
         public void Coin()
         {
             //created an object of class Random
-            Random ObjRandom = new Random();
-            int check = ObjRandom.Next(0, 2);
+            Random objRandom = new Random();
+            int check = objRandom.Next(0, 2);
             if (check < 0.5)
             {
                 Console.WriteLine("Winner is Head");
